5b41cf48fccd03842f883ba0895b7d877deaf15c
Meljska-Fawn <meli.explores@gmail.com>
2023-05-20 10:47:29 -0700

    added files and structure, container and navbar code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/PortfolioContainer.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import Resume from './pages/Resume';
+import About from './pages/About';
+import Work from './pages/Work';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState('About');
+
+    const renderPage = () => {
+        if (currentPage === 'About') {
+            return <About />;
+        }
+        if (currentPage === 'Work') {
+            return <Work />;
+        }
+        if (currentPage === 'Resume') {
+            return <Resume />;
+        }
+        return <Contact />;
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+            {renderPage()}
+        </div>
+    );
+}
