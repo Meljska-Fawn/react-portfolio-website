3a3927458d709afb828da4935626ef3c93c18126
Meljska-Fawn <meli.explores@gmail.com>
2023-05-20 10:50:20 -0700

    updated app.js code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import Header from './components/Header';
-import './App.css';
+import PortfolioContainer from "./components/PortfolioContainer";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Navbar />
-    </div>
-  );
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
