6980dbbb07f687cfbcb31b6328902bd318f7cfd1
Meljska-Fawn <meli.explores@gmail.com>
2023-05-21 12:44:26 -0700

    added content to pages and fixed export functions

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,27 +1,26 @@
 import React from 'react';
+import { Container } from "@mui/system";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
 
-function Footer() {
+const Footer = () => {
   return (
-    <div>
-      <div>
-        <span>
+    <AppBar>
+      <Container>
+        <Toolbar>
           <a href="https://github.com/Meljska-Fawn"><GitHubIcon fontSize="large"/></a>
-        </span>
-        <span>
           <a href="https://www.linkedin.com/in/melissa-vaughn-663b35226/">
             <LinkedInIcon fontSize="large"/>
           </a>
-        </span>
-        <span>
           <a href="https://www.instagram.com/melissa.lavaughn/">
             <InstagramIcon fontSize="large"/>
           </a>
-        </span>
-      </div>
-    </div>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 // Here we are importing a CSS file as a dependency
 
 
-function Header() {
+const Header = () => {
   return (
     <header className="header">
       <h1>Melissa Vaughn</h1>
